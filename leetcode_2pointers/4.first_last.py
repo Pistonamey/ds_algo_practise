ab235db51ded768c30d8b4514f31702ce277f30b
Pistonamey <ameyshinde11111@gmail.com>
2023-06-21 21:31:35 -0500

    find first and last occurence of an element in the array

---
+++ leetcode_2pointers/4.first_last.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        return [self.first_occurrence(nums, target), self.last_occurrence(nums, target)]
+    
+    def first_occurrence(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        if l < len(nums) and nums[l] == target:
+            return l
+        return -1
+    
+    def last_occurrence(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        if r >= 0 and nums[r] == target:
+            return r
+        return -1
