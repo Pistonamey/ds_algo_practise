f281bf1beaa217a7882b32d7b45a15cae37a750e
Pistonamey <ameyshinde11111@gmail.com>
2023-12-18 00:28:19 +0530

    top k

---
+++ 10. top k/problem1.py
@@ -0,0 +1,22 @@
+# return the top K largest elements from the unsorted array
+from heapq import *
+
+def top_k_largest(nums,k):
+    result=[]
+    heapify(result)
+    for i in range(k):
+        heappush(result,nums[i])
+    
+    for i in range(k,len(nums)):
+        if(nums[i]>result[0]):
+            heappop(result)
+            heappush(result,nums[i])
+    
+
+
+    return result
+
+def main():
+    print("Here are the top K elements: "+str(top_k_largest([3,1,5,12,2,11],3)))
+
+main()
